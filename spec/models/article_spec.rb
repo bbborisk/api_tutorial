195a28f35934007d5bb921ea6d46af100af63df6
bbborisk <bbborisk@gmail.com>
2020-03-19 21:28:16 +0100

    added validations and tests

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+
+describe '#validations' do
+
+
+  it 'should test that factory is valid' do
+    expect(FactoryBot.build :article).to be_valid
+  end
+
+  it 'should validate presence of the title' do
+    article = FactoryBot.build :article, title: ''
+    expect(article).not_to be_valid
+    expect(article.errors.messages[:title]).to include("can't be blank")
+  end
+
+  it 'should validate presence of the content' do
+    article = FactoryBot.build :article, content: ''
+    expect(article).not_to be_valid
+    expect(article.errors.messages[:content]).to include("can't be blank")
+  end
+
+  it 'should validate presence of the slug' do
+    article = FactoryBot.build :article, slug: ''
+    expect(article).not_to be_valid
+    expect(article.errors.messages[:slug]).to include("can't be blank")
+  end
+
+  it 'should validate uniqueness of the slug' do
+    article = FactoryBot.create :article
+    invalid_article = FactoryBot.build :article, slug: article.slug
+    expect(invalid_article).not_to be_valid
+  end
+end
+end
