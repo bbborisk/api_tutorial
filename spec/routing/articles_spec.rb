7b3c10d61b21205b43966f3f6cc5df671ea7d6a0
bbborisk <bbborisk@gmail.com>
2020-03-23 16:19:12 +0100

    Test commit

--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -8,4 +8,8 @@ describe 'article routes' do
   it 'should rout to articles show' do
     expect(get '/articles/1').to route_to('articles#show', id:'1')
   end
+
+  it 'should rout to articles show' do
+    expect(get '/articles/2').to route_to('articles#show', id:'2')
+  end
 end
