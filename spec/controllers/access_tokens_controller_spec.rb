c290c44376b62c9de232a3449ddf7e00982ca90e
bbborisk <bbborisk@gmail.com>
2020-05-18 13:55:27 +0200

    Fixed github octokit trouble with code

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe AccessTokensController, type: :controller do
   describe "#create" do
-    context "when invalid request" do
+    shared_examples_for "unauthorized_requests" do
       let(:error) do
         {
           "status" => "401",
@@ -13,15 +13,29 @@ RSpec.describe AccessTokensController, type: :controller do
 
       end
       it "should return 401 status" do
-        post :create
+        subject
         expect(response).to have_http_status(401)
       end
       it "should return propper error msg " do
-        post :create
+        subject
         expect(json['errors']).to include(error)
       end
     end
 
+    context "when no code provided" do
+      subject{ post :create }
+      it_behaves_like "unauthorized_requests"
+    end
+
+    context "when invalid code provided" do
+      let(:github_errors) {double("Sawyer::Resource", error: "bad_verification")}
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(:exchange_code_for_token).and_return(github_errors)
+      end
+
+      subject{ post :create, params: { code: 'invalid_code'}}
+      it_behaves_like "unauthorized_requests"
+    end
     context "when succesfull request" do
 
     end
